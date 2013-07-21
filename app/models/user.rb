564325dc74f11100e6b591767731ea7cfed78d7d
Cassandra King <puddincat@gmail.com>
2013-07-21 19:37:18 -0400

    Add index to users email in db migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
   attr_accessible :email, :name
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A{\w+\_.}+@{a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\d\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
 end
