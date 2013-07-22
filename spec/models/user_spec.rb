0e132c2ccb68eedf2ddaf061b9d28abc3c2fc05e
Cassandra King <puddincat@gmail.com>
2013-07-21 20:18:35 -0400

    add password and confirmation, start

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
 
 before do 
- @user = User.new(name: "Example User", email: "user@example.com") 
+ @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") 
 end
 
 
@@ -11,6 +11,7 @@ subject { @user }
 
 it {should respond_to(:name) }
 it {should respond_to(:email) }
+it {should respond_to(:password_digest)}
 
 it {should be_valid }
 
