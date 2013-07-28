194a268e0ca17404929582495d1c12863223d5bd
Cassandra King <puddincat@gmail.com>
2013-07-28 15:33:09 -0400

    begin user signup and rspec capybara testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SampleApp::Application.routes.draw do
+	resources :users
 
-  get "users/new"
+  # get "users/new"
 
   root to: 'static_pages#home'
 
-  match '/signup', to: 'users#new'
+  match '/signup', to: 'users#new', via: 'get'
 
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+	factory :user do
+
+	name "Cassandra King"
+	email "puddincat@gmail.com"
+	password "foobar"
+	password_confirmation "foobar"
+	end
+end
