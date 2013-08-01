28f88bb38ab755a83b68a2eaca8b7ef0fa63bf3a
Cassandra King <puddincat@gmail.com>
2013-08-01 16:44:11 -0400

    Finish user signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+#gain access to MD5 hashes for gravatar use
+#fix found here: http://stackoverflow.com/questions/770876/how-do-i-add-gravatar-identicons-into-ruby-on-rails
+		# create an MD5 hash from the user's email address which is then used in a GET request to the gravatar server
+require 'digest'
