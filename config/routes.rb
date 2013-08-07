d7b47332d769347c1825bec7f9eab034037313ca
Cassandra King <puddincat@gmail.com>
2013-08-07 16:29:18 -0400

    begin adding details regarding sign up and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 SampleApp::Application.routes.draw do
 	resources :users
+	resources :sessions,  only: [:new, :create, :destroy]
 
-  # get "users/new"
+root to: 'static_pages#home'
 
-  root to: 'static_pages#home'
+  match '/signup', 	to: 'users#new', via: 'get'
+   match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
-  match '/signup', to: 'users#new', via: 'get'
-
-  match '/help', to: 'static_pages#help'
-  match '/about', to: 'static_pages#about'
+  match '/help', 	to: 'static_pages#help'
+  match '/about', 	to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  match '/home', to: 'static_pages#home'
-
-
+  match '/home', 	to: 'static_pages#home'
 
 end
 
